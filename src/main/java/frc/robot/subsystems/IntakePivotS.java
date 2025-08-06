98f5286d4991437d071ed1694e357fd1f4d51192
Arik Petracci <kakapobird6@gmail.com>
2025-08-06 10:16:33 -0700

    added intake pivot soft limits

--- src/main/java/frc/robot/subsystems/IntakePivotS.java
+++ src/main/java/frc/robot/subsystems/IntakePivotS.java
@@ -11,6 +11,7 @@ import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.controls.VoltageOut;
 
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -19,18 +20,29 @@ public class IntakePivotS extends SubsystemBase {
   public class IntakePivotConstants {
 
     public static final int INTAKE_PIVOT_MOTOR_CAN_ID = 40;
-    public static final double INTAKE_PIVOT_UP_VOLTAGE = 0.5; // Voltage to move the intake pivot up
-    public static final double INTAKE_PIVOT_DOWN_VOLTAGE = -0.5; // Voltage to move the intake pivot down
+    public static final double INTAKE_PIVOT_UP_VOLTAGE = 2.0; // Voltage to move the intake pivot up
+    public static final double INTAKE_PIVOT_DOWN_VOLTAGE = -2.0; // Voltage to move the intake pivot down
+
+    public static final double FORWARD_SOFT_LIMIT = Units.degreesToRotations(40.0);
+    public static final double REVERSE_SOFT_LIMIT = Units.degreesToRotations(-40.0);
 
   }
 
   private final TalonFX IntakePivotMotor = new TalonFX(IntakePivotConstants.INTAKE_PIVOT_MOTOR_CAN_ID);
+  private final TalonFXConfiguration motorConfigs = new TalonFXConfiguration();
 
   public IntakePivotS() {
-
-    IntakePivotMotor.getConfigurator().apply(new TalonFXConfiguration());
+    
+    setMotorConfigs();
+    IntakePivotMotor.getConfigurator().apply(motorConfigs);
+  
     setDefaultCommand(stop());
 
+  }
+
+  private void setMotorConfigs() {
+    motorConfigs.SoftwareLimitSwitch.ForwardSoftLimitThreshold = IntakePivotConstants.FORWARD_SOFT_LIMIT;
+    motorConfigs.SoftwareLimitSwitch.ReverseSoftLimitThreshold = IntakePivotConstants.REVERSE_SOFT_LIMIT;
   }
 
   public Command voltage(double voltage) {
