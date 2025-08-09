857cbdd5fd4d155e1afafd6fd43825a176ed76f9
Jeremy <maplecake199@gmail.com>
2025-08-09 16:11:02 -0700

    Removed duplicate command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -45,15 +45,8 @@ public class RobotContainer {
     public RobotContainer() {
         configureBindings();
     }
-
-
-    public Command Intake() {
-        return Commands.parallel(intakePivot.slapDown(),intakeRoller.intakeRollers())
-                .until(() -> intakeRoller.getCurrent() > 40).andThen(intakePivot.slapUp());
-    }
-
-
-    private void configureBindings() {
+    
+       private void configureBindings() {
         // Note that X is defined as forward according to WPILib convention,
         // and Y is defined as to the left according to WPILib convention.
         drivetrain.setDefaultCommand(
