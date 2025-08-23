8e6d425bf554c74bf16228a1de28c36ebf82df66
hsumt <matthew.t.hsu@gmail.com>
2025-08-23 13:22:13 -0700

    Initialized choreo trajectories for 3 piece

--- src/main/java/frc/robot/Autos.java
+++ src/main/java/frc/robot/Autos.java
@@ -1,5 +1,15 @@
 package frc.robot;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
+import frc.robot.subsystems.ArmS;
+import frc.robot.subsystems.ElevatorS;
+import frc.robot.subsystems.HandS;
+import frc.robot.subsystems.IntakePivotS;
+import frc.robot.subsystems.IntakeRollerS;
 
 public class Autos {
-    
+    protected final CommandSwerveDrivetrain m_drivebase;
+    protected final ArmS m_arm;
+    protected final IntakePivotS m_intakepiv;
+    protected final IntakeRollerS m_intakerol;
+    protected final 
 }
