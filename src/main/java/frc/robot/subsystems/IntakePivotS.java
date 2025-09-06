785a8c0272498452d258f326d551374745099240
Jeremy <maplecake199@gmail.com>
2025-09-06 14:06:01 -0700

    Working feedforward

--- src/main/java/frc/robot/subsystems/IntakePivotS.java
+++ src/main/java/frc/robot/subsystems/IntakePivotS.java
@@ -33,6 +33,8 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.generated.TunerConstants;
+
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.controls.PositionTorqueCurrentFOC;
 
 public class IntakePivotS extends SubsystemBase {
@@ -50,13 +52,6 @@ public class IntakePivotS extends SubsystemBase {
 
     //Constants used for PID and Feedforward
     public static final double MOTOR_ROTATIONS_PER_PIVOT_ROTATION = 12.5;
-    public static final double kArmP = 6; // Talon FX PID P gain (tune this) 6
-    public static final double kArmI = 0; // Talon FX PID I gain (tune this)
-    public static final double kArmD = 0.7; // Talon FX PID D gain (tune this) 0.4
-    public static final double kArmS = -0.05; // Feedforward Static gain (tune this) -0.05
-    public static final double kArmG = 0.9; // Feedforward Gravity gain (tune this)
-    public static final double kArmV = 0; // Feedforward Velocity gain (tune this)
-    public static final double kArmA = 0; // Feedforward Acceleration gain (tune this)
 
     //Pivot Offset from Zero degrees (when the code starts, it always resets the angle to zero so this is neccesary
     // for offseting it to the upper hard stop)
@@ -66,8 +61,7 @@ public class IntakePivotS extends SubsystemBase {
     public static final double kArmGearRatio = kSensorToMechanismRatio; // For clarity, same as above
  
     //Initiallizes the feedforward controller to a variable
-    private static final ArmFeedforward intakeFeedforward = new ArmFeedforward(
-        kArmS, kArmG, kArmV, kArmA);
+
    
     //Motor configuration
     private static TalonFXConfiguration configureMotor(TalonFXConfiguration config) {
@@ -77,80 +71,92 @@ public class IntakePivotS extends SubsystemBase {
       config.CurrentLimits.withSupplyCurrentLimitEnable(true).withSupplyCurrentLimit(Amps.of(50));
       config.Feedback.withSensorToMechanismRatio(MOTOR_ROTATIONS_PER_PIVOT_ROTATION);
       config.Slot0 = new Slot0Configs();
-
-      return config;
-    }
-  }
-
-  //Other constants
-
-  //Sets initial target angle to hard stop
-  public static double targetAngle = 141;
-
-  //Initiallizes the PID controller to a variable
-  private static PIDController m_pidController = 
-  new PIDController(
-      IntakePivotConstants.kArmP,
-      IntakePivotConstants.kArmI,
-      IntakePivotConstants.kArmD
-
-  );
-
-  //Sets up visualization 
-  private final NetworkTableInstance inst = NetworkTableInstance.getDefault();
-
-  //Sets the motor to the CAN ID and CAN Bus
-  private static final TalonFX IntakePivotMotor = new TalonFX(IntakePivotConstants.INTAKE_PIVOT_MOTOR_CAN_ID,
-      TunerConstants.kCANBus2);
-
-  //Sets up a simulation visualizer for our mechanism
-public final MechanismLigament2d IntakePivotVisualizer = new MechanismLigament2d("Intake", 1, 0); 
-
-  //set initiall configurations/values
-  public IntakePivotS() {
-
-    var config = new TalonFXConfiguration();
-
-    IntakePivotMotor.getConfigurator().refresh(config);
-    IntakePivotMotor.getConfigurator().apply(IntakePivotConstants.configureMotor(config));
-
-    SignalLogger.start();
-  }
-
-  //Commands:
-
-  //Takes in an angle, and sets the PID target angle to that angle
-  public Command moveToAngle(double someAngle) {
-    return run(() -> {
-      targetAngle = someAngle;
-   
-    });
-  }
-
-  //Periodic (always active):
-  @Override
-  public void periodic() {
-
-    //Puts values to Smart Dashboard. Add as needed for simulation
-    SmartDashboard.putNumber("Intake/TargetAngle", targetAngle);
-    SmartDashboard.putNumber("Intake/currentAngleRadians", getArmAngleRadians());
-    SmartDashboard.putNumber("supplycurrent", IntakePivotMotor.getSupplyCurrent().getValueAsDouble());
-    SmartDashboard.putNumber("statorcurrent", IntakePivotMotor.getStatorCurrent().getValueAsDouble());
-    SmartDashboard.putNumber("volage", IntakePivotMotor.getMotorVoltage().getValueAsDouble());
-
-    //sets initiall angle for simulation
-    IntakePivotVisualizer.setAngle(new Rotation2d(Degrees.of(getArmAngleRadians() * 180/Math.PI)));
-
-    //Sets the voltage of the motor to the sum of Feedforward and PID controllers
-      IntakePivotMotor.setVoltage(IntakePivotConstants.intakeFeedforward.calculate(
-      getArmAngleRadians(), 1, 1) +
-    m_pidController.calculate(getArmAngleRadians(), (targetAngle * (Math.PI/180))));
-  }
-
-  //Methods:
-  //Gets the output of the motor sensor, then converts it to the accurate radian measure for the pivot
-  public double getArmAngleRadians() {
-    return (IntakePivotMotor.getRotorPosition().getValueAsDouble() / IntakePivotConstants.kArmGearRatio) * 2 * Math.PI + IntakePivotConstants.kArmOffset;
+      var slot0Configs = config.Slot0;
+           // slot0Configs.kG = getArmFeedforward(); //Gravity compensation
+                        slot0Configs.kS = 0.05; // Add 0.25 V output to overcome static friction
+                        slot0Configs.kV = 0; // A velocity target of 1 rps results in 0.12 V output
+                        slot0Configs.kA = 0; // An acceleration of 1 rps/s requires 0.01 V output
+                        slot0Configs.kP = 0.1; // A position error of 2.5 rotations results in 12 V output
+                        slot0Configs.kI = 0; // no output for integrated error
+                        slot0Configs.kD = 0; // A velocity error of 1 rps results in 0.1 V output
+                  
+                        var motionMagicConfigs = config.MotionMagic;
+                        motionMagicConfigs.MotionMagicCruiseVelocity = 0; // Target cruise velocity of 80 rps
+                        motionMagicConfigs.MotionMagicAcceleration = 0; // Target acceleration of 160 rps/s (0.5 seconds)
+                        motionMagicConfigs.MotionMagicJerk = 0; // Target jerk of 1600 rps/s/s (0.1 seconds)
+                  
+                        return config;
+                      }
+                    }
+                  
+                    //Other constants
+                  
+                    //Sets initial target angle to hard stop
+                    public static double targetAngle = 141;
+                  
+                    //Initiallizes the PID controller to a variable
+                  
+                  
+                    //Sets up visualization 
+                    private final NetworkTableInstance inst = NetworkTableInstance.getDefault();
+                  
+                    //Sets the motor to the CAN ID and CAN Bus
+                    private static final TalonFX IntakePivotMotor = new TalonFX(IntakePivotConstants.INTAKE_PIVOT_MOTOR_CAN_ID,
+                        TunerConstants.kCANBus2);
+                  
+                    //Sets up a simulation visualizer for our mechanism
+                  public final MechanismLigament2d IntakePivotVisualizer = new MechanismLigament2d("Intake", 1, 0); 
+                  
+                    //set initiall configurations/values
+                    public IntakePivotS() {
+                  
+                      var config = new TalonFXConfiguration();
+                  
+                      IntakePivotMotor.getConfigurator().refresh(config);
+                      IntakePivotMotor.getConfigurator().apply(IntakePivotConstants.configureMotor(config));
+                  
+                      SignalLogger.start();
+                    }
+                  
+                    //Commands:
+                  
+                    //Takes in an angle, and sets the PID target angle to that angle
+                    public Command moveToAngle(double someAngle) {
+                      return run(() -> {
+                        targetAngle = someAngle;
+                     
+                      });
+                    }
+                  
+                    //Periodic (always active):
+                    @Override
+                    public void periodic() {
+                  
+                      //Puts values to Smart Dashboard. Add as needed for simulation
+                      SmartDashboard.putNumber("Intake/TargetAngle", targetAngle);
+                      SmartDashboard.putNumber("Intake/currentAngleRadians", getArmAngleRadians());
+                      SmartDashboard.putNumber("supplycurrent", IntakePivotMotor.getSupplyCurrent().getValueAsDouble());
+                      SmartDashboard.putNumber("statorcurrent", IntakePivotMotor.getStatorCurrent().getValueAsDouble());
+                      SmartDashboard.putNumber("volage", IntakePivotMotor.getMotorVoltage().getValueAsDouble());
+                  
+                      //sets initiall angle for simulation
+                      IntakePivotVisualizer.setAngle(new Rotation2d(Degrees.of(getArmAngleRadians() * 180/Math.PI)));
+                  
+                      // create a Motion Magic request, voltage output
+                      final MotionMagicVoltage m_request = new MotionMagicVoltage(0).withFeedForward(getArmFeedforward());
+                  
+                  // set target position to 100 rotations
+                      IntakePivotMotor.setControl(m_request.withPosition(-15));
+                    }
+                  
+                    //Methods:
+                    //Gets the output of the motor sensor, then converts it to the accurate radian measure for the pivot
+                    public double getArmAngleRadians() {
+                return (IntakePivotMotor.getRotorPosition().getValueAsDouble() / IntakePivotConstants.kArmGearRatio) * 2 * Math.PI + IntakePivotConstants.kArmOffset;
+              }
+            
+              public double getArmFeedforward() {
+            return 0.95 * Math.cos(IntakePivotMotor.getRotorPosition().getValueAsDouble() / IntakePivotConstants.kArmGearRatio * 2 * Math.PI + IntakePivotConstants.kArmOffset);
   }
 
 }
