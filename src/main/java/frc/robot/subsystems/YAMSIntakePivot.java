7d60dd1c9d95cfe324a423a203aaae287fe769d6
Jeremy <maplecake199@gmail.com>
2025-09-19 17:42:38 -0700

    YAMS Intial Intake Code

--- src/main/java/frc/robot/subsystems/YAMSIntakePivot.java
+++ src/main/java/frc/robot/subsystems/YAMSIntakePivot.java
@@ -47,18 +47,18 @@ public class YAMSIntakePivot extends SubsystemBase {
   private SmartMotorControllerConfig smcConfig = new SmartMotorControllerConfig(this)
   .withControlMode(ControlMode.CLOSED_LOOP)
   // Feedback Constants (PID Constants)
-  .withClosedLoopController(6, 0, 0.5, DegreesPerSecond.of(180), DegreesPerSecondPerSecond.of(280))
+  .withClosedLoopController(10, 0, 0.5, DegreesPerSecond.of(600), DegreesPerSecondPerSecond.of(688))
   .withSimClosedLoopController(10, 0, 0.5, DegreesPerSecond.of(458), DegreesPerSecondPerSecond.of(688))
   // Feedforward Constants
-  .withFeedforward(new ArmFeedforward(0, 0.9, 0))
-  .withSimFeedforward(new ArmFeedforward(0,1.0, 0))
+  .withFeedforward(new ArmFeedforward(0, 1.0, 0))
+  .withSimFeedforward(new ArmFeedforward(0, 1.0, 0))
   // Telemetry name and verbosity level
   .withTelemetry("ArmMotor", TelemetryVerbosity.HIGH)
   // Gearing from the motor rotor to final shaft.
   // In this example gearbox(3,4) is the same as gearbox("3:1","4:1") which corresponds to the gearbox attached to your motor.
   .withGearing(SmartMechanism.gearing(SmartMechanism.gearbox(12.5,1)))
   // Motor properties to prevent over currenting.
-  .withMotorInverted(false)
+  .withMotorInverted(true)
   .withIdleMode(MotorMode.BRAKE)
   .withStatorCurrentLimit(Amps.of(120));
 
