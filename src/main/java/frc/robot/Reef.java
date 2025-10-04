4cb4ad01a5c75ac45d0bc38a98a6df04f8957522
hsumt <matthew.t.hsu@gmail.com>
2025-10-03 18:15:18 -0700

    Updated Trajectories

--- src/main/java/frc/robot/Reef.java
+++ src/main/java/frc/robot/Reef.java
@@ -6,18 +6,19 @@ import edu.wpi.first.math.geometry.Rotation2d;
 public class Reef {
         // 12 reef faces
     public static final Pose2d[] FACES = {
-        new Pose2d(3.02, 4.19, Rotation2d.fromDegrees(270)), //A
-        new Pose2d(3.02, 3.87, Rotation2d.fromDegrees(270)), //B
-        new Pose2d(3.62, 2.84, Rotation2d.fromDegrees(330)), //C
-        new Pose2d(3.90, 2.68, Rotation2d.fromDegrees(330)), //D
-        new Pose2d(5.08, 2.68, Rotation2d.fromDegrees(30)),  //E
-        new Pose2d(5.36, 2.84, Rotation2d.fromDegrees(30)),  //F
-        new Pose2d(5.96, 3.87, Rotation2d.fromDegrees(90)),  //G
-        new Pose2d(5.96, 4.19, Rotation2d.fromDegrees(90)),  //H
-        new Pose2d(5.36, 5.21, Rotation2d.fromDegrees(150)), //I
-        new Pose2d(5.08, 5.37, Rotation2d.fromDegrees(150)), //J
-        new Pose2d(3.90, 5.37, Rotation2d.fromDegrees(210)), //K
-        new Pose2d(3.62, 5.21, Rotation2d.fromDegrees(210))  //L
+        ChoreoVariables.getPose("A"), //A
+        ChoreoVariables.getPose("B"), //B
+        ChoreoVariables.getPose("C"), //C
+        ChoreoVariables.getPose("D"), //D
+        ChoreoVariables.getPose("E"), //E
+        ChoreoVariables.getPose("F"), //F
+        ChoreoVariables.getPose("G"), //G
+        ChoreoVariables.getPose("H"), //H
+        ChoreoVariables.getPose("I"), //I
+        ChoreoVariables.getPose("J"), //J
+        ChoreoVariables.getPose("K"), //K
+        ChoreoVariables.getPose("L"), //L
+
     };
     public enum ReefSide {
         R1(FACES[0], "left"),
@@ -42,18 +43,7 @@ public class Reef {
         }
     }
 
-    public static ReefSide closestSide(Pose2d robotPose) {
-        ReefSide closest = ReefSide.R1;
-        double minDistance = Double.MAX_VALUE;
-        for (ReefSide side : ReefSide.values()) {
-            double distance = robotPose.getTranslation().getDistance(side.pose.getTranslation());
-            if (distance < minDistance) {
-                minDistance = distance;
-                closest = side;
-            }
-        }
-        return closest;
-    }
+
     public static ReefSide closestSide(Pose2d robotPose, String branch) {
         ReefSide closest = null;
         double minDistance = Double.MAX_VALUE;
